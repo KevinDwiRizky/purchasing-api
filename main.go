cac4abd1910aa03519073210467afdab3c622e92
Kevin Dwi Rizky <kevindwirizkyy@gmail.com>
2026-01-20 10:45:34 +0700

    create model and migration

--- main.go
+++ main.go
@@ -4,13 +4,14 @@ import (
 	"log"
 	"os"
 	"purchasing-api/config"
+	"purchasing-api/models"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	// Load environment variables from .env file
+	
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found, using system environment variables")
 	}
@@ -19,6 +20,15 @@ func main() {
 
 	config.ConnectDB()
 
+	config.DB.AutoMigrate(
+		&models.User{},
+		&models.Supplier{},
+		&models.Item{},
+		&models.Purchasing{},
+		&models.PurchasingDetail{},
+	)
+
+	
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("API + DB connected")
 	})
